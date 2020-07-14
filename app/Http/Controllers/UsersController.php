6b071fd322ec69c09d278b67476eaecc4fd8d285
lgbya <lgbya1105882406@aliyun.com>
2020-07-15 01:21:53 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,13 +7,26 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    public function create()
-    {
-        return view('users.create');
-    }
 
     public function show(User $user)
     {
         return view('users.show', ['user'=>$user]);
     }
+
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+        return;
+    }
+
 }
