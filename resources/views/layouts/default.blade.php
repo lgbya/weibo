5424b94da59c1cc821f2a3ba0a87562635d798c7
lgbya <lgbya1105882406@aliyun.com>
2020-07-15 03:01:32 +0800

    中文的错误提示信息

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>@yield('title', 'Weibo App')</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
 </head>
 <body>
 @include('layouts._header')
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -11,8 +11,7 @@
                 @include('shared._errors')
 
                 <form method="POST" action="{{ route('users.store') }}">
-                    {{csrf_field()}}
-
+                    {{ csrf_field() }}
                     <div class="form-group">
                         <label for="name">名称:</label>
                         <input type="text" name="name" class="form-control" value="{{ old('name') }}">
