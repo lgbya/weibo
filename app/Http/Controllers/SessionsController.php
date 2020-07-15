36751b5a797eff34d41c67f476fe89140b674ba7
lgbya <lgbya1105882406@aliyun.com>
2020-07-15 09:35:40 +0800

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -19,7 +19,7 @@ class SessionsController extends Controller
             'password'=> 'required',
         ]);
 
-        if(!Auth::attempt($credentials)){
+        if(!Auth::attempt($credentials, $request->has('remember'))){
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
