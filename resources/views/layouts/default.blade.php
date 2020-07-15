6ab140e30f959aff3dc5c82956ef00813de03602
lgbya <lgbya1105882406@aliyun.com>
2020-07-15 09:20:39 +0800

    登录成功后导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,14 +3,16 @@
 <head>
     <title>@yield('title', 'Weibo App')</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
 </head>
 <body>
 @include('layouts._header')
 <div class="container">
-    @include('shared._messages')
-    @yield('content')
-    @include('layouts._footer')
+    <div  class="offset-md-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </div>
+<script src="{{mix('js/app.js')}}"></script>
 </body>
 </html>
